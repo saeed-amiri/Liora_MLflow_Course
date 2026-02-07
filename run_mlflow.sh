765a8a34f26f3673c3ecb277d87dc6623384773f
saeed amiri <sdramiri952@gmail.com>
2026-02-07 15:57:16 +0100

    Commands to run mlflow

---
+++ run_mlflow.sh
@@ -0,0 +1,6 @@
+mlflow server   \
+    --host 0.0.0.0 \
+    --port 8080 \
+    --backend-store-uri file:///home/santiago/Documents/MyScripts/dst_flows/mlflow_dst/Liora_MLflow_Course/mlruns \
+    --default-artifact-root file:///home/santiago/Documents/MyScripts/dst_flows/mlflow_dst/Liora_MLflow_Course/mlruns \
+    --serve-artifacts
